2af99429a666fefeecc4496b143b448408ab53c5
AlexisF27 <alexisf_27@hotmail.com>
2026-07-13 00:34:42 +0100

    useProduct custom hook

--- src/components-patterns/components/ProductCard.tsx
+++ src/components-patterns/components/ProductCard.tsx
@@ -1,15 +1,10 @@
-import React, { useState } from 'react'
 import styles from '../styles/styles.module.css'
 import noImage from '../assets/no-image.jpg'
+import { useProduct } from '../hooks/useProduct'
 
 const ProductCard = () => {
 
-    const [counter, setCounter] = useState(0);
-
-    const increaseBy = (value: number) => {
-        setCounter(prev => Math.max(prev + value, 0))
-
-    }
+    const { counter, increaseBy } = useProduct(0);
 
     return (
         <>
@@ -27,6 +22,11 @@ const ProductCard = () => {
                 <div className={styles.productCard}>
                     <img src={noImage} alt="Coffe Mug" className={styles.productImg} />
                     <h1>Product Card</h1>
+                    <div className={styles.buttonsContainer}>
+                        <button onClick={() => increaseBy(-1)} className={styles.buttonMinus}>-</button>
+                        <div className={styles.countLabel}>{counter}</div>
+                        <button onClick={() => increaseBy(1)} className={styles.buttonAdd}>+</button>
+                    </div>
                 </div>
             </div>
         </>
