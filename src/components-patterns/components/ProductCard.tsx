a700414cdc19b10b6e1fcfd16f2891be4d78b531
AlexisF27 <alexisf_27@hotmail.com>
2026-07-13 19:08:50 +0100

    Compnent ProductCard being atomic

--- src/components-patterns/components/ProductCard.tsx
+++ src/components-patterns/components/ProductCard.tsx
@@ -1,17 +1,24 @@
 import styles from '../styles/styles.module.css'
-import noImage from '../assets/no-image.jpg'
+
 import { useProduct } from '../hooks/useProduct'
 
-const ProductCard = () => {
+interface ProductCardProps {
+    image: string;
+    title: string;
+}
+
+
+const ProductCard = ({ image, title }: ProductCardProps) => {
 
     const { counter, increaseBy } = useProduct(0);
+
 
     return (
         <>
             <div style={{ display: 'flex' }} >
                 <div className={styles.productCard}>
-                    <img src='./coffee-mug.png' alt="Coffe Mug" className={styles.productImg} />
-                    <span className={styles.productDescription}> Coffe Mug</span>
+                    <img src={image} alt={title} className={styles.productImg} />
+                    <span className={styles.productDescription}> {title}</span>
                     <div className={styles.buttonsContainer}>
                         <button onClick={() => increaseBy(-1)} className={styles.buttonMinus}>-</button>
                         <div className={styles.countLabel}>{counter}</div>
@@ -19,15 +26,6 @@ const ProductCard = () => {
                     </div>
                 </div>
 
-                <div className={styles.productCard}>
-                    <img src={noImage} alt="Coffe Mug" className={styles.productImg} />
-                    <h1>Product Card</h1>
-                    <div className={styles.buttonsContainer}>
-                        <button onClick={() => increaseBy(-1)} className={styles.buttonMinus}>-</button>
-                        <div className={styles.countLabel}>{counter}</div>
-                        <button onClick={() => increaseBy(1)} className={styles.buttonAdd}>+</button>
-                    </div>
-                </div>
             </div>
         </>
     )
--- src/components-patterns/pages/ShoppingPage.tsx
+++ src/components-patterns/pages/ShoppingPage.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
 import ProductCard from "../components/ProductCard"
+import noImage from '../assets/no-image.jpg'
 
 const ShoppingPage = () => {
     return (
         <div>
             <h1>Shopping Store</h1>
             <span></span>
-            <ProductCard></ProductCard>
+            <div style={{ display: 'flex' }}>
+                <ProductCard image="/coffee-mug.png" title="Coffe Mug"></ProductCard>
+                <ProductCard image={noImage} title="Product Card"></ProductCard>
+            </div>
         </div>
     )
 }
